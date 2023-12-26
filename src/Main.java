865a247aee785cf7129d4a2887bca1d8eece1e82
Aastapchik <154362326+Aastapchik@users.noreply.github.com>
2023-12-26 22:25:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,51 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.print("Введите имя пользователя: ");
         Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
-        System.out.print("Hello " + username);
+
+        //1 задача
+        int k = scanner.nextInt();
+        if (k % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+
+        //2 задача
+        int t = scanner.nextInt();
+        if (t > -5) {
+            System.out.println("Warm");
+        } else if (t <= -5 && t > -20) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Cold");
+        }
+
+        //3 задача
+        System.out.println("Квадраты чисел от 10 до 20: ");
+        for (int i = 10; i <= 20; i++)
+            System.out.print(i * i + " ");
+
+        //4 задача
+        int q = 1;
+        while (q < 15) {
+            System.out.print(7 * q + " ");
+            q++;
+        }
+        System.out.println(" ");
+
+        //* задача
+
+        System.out.print("Введите целое положительно число: ");
+        if (scanner.hasNextInt()) {
+            int h = scanner.nextInt();
+            if (h > 0) {
+                System.out.println("Сумма равна: " + (h * (h + 1) / 2));
+            }
+        } else {
+            System.out.println("Число не подходит по критериям");
+        }
+        scanner.close();
     }
 }
